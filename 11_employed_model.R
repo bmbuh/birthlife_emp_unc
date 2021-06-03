999d337e191c29e60e25fee0568307bef5f71667
Brian Buh <77400817+bmbuh@users.noreply.github.com>
2021-06-03 19:02:24 +0200

    Commit mid day 03.06.2021

--- 11_employed_model.R
+++ 11_employed_model.R
@@ -1,6 +1,6 @@
 #Coded by: Brian Buh
 #Started on: 26.05.2021
-#Last Updated: 27.05.2021
+#Last Updated: 03.06.2021
 
 surv5 <- file.choose()
 surv5 <- readRDS(surv5)
@@ -47,30 +47,67 @@ survemp <-  surv5 %>%
   mutate(timecheck = t2 - t2_2) %>% 
   dplyr::select(-finfut.num, -finnow.num) %>% 
   mutate(t2_3 = ifelse(timecheck >= 0, t2, t2_2)) %>% 
-  mutate(t2_3 = ifelse(is.na(t2_3), t2, t2_3))  #This final number ensures that the timing of the conception is correct
-
-survemp %>% count(parttime)
+  mutate(t2_3 = ifelse(is.na(t2_3), t2, t2_3))  %>% #This final number ensures that the timing of the conception is correct
+  left_join(., imp_permcon, by = c("pidp", "wave")) #This dataframe is created a bit further down but retrospectively added here to ensure there is not issues
+  
+survemp %>% count(permcon)
+survemp %>% count(permcon.imp)
+survemp2 %>% count(permcon)
 
 #The objective job measure variables have ~8% missing values.
 #I do a simple imputation here to see if it is worth it to do a more advanced one.
-survemp2 <- survemp %>% 
-  fill(permcon, .direction = "up") %>% 
-  fill(parttime, .direction = "up") %>% 
-  fill(jbsat, .direction = "up") %>% 
-  fill(jbpl, .direction = "up") %>% 
-  mutate(jbpl = as.character(jbpl)) %>% 
-  fill(priv, .direction = "up")
+# survemp2 <- survemp %>% 
+#   fill(permcon, .direction = "up") %>% 
+#   fill(parttime, .direction = "up") %>% 
+#   fill(jbsat, .direction = "up") %>% 
+#   fill(jbpl, .direction = "up") %>% 
+#   mutate(jbpl = as.character(jbpl)) %>% 
+#   fill(priv, .direction = "up")
 #It appears that only permcon has a significant effect.  
 
-survemp2 %>% count(jbpl)
+#Impute missing values into permcon
+wide_permcon <- survemp %>% 
+  dplyr::select(pidp, wave, permcon) %>% 
+  # mutate(finnow = as.factor(finnow)) %>% 
+  mutate(wn = "w") %>% 
+  #mutate(finnow = ifelse(finnow <= -1, NA, finnow)) %>% 
+  unite(wavenum, wn, wave, sep = "", remove = TRUE) %>% 
+  pivot_wider(names_from = wavenum, values_from = permcon) %>% 
+  mutate_if(is.numeric, as.factor) %>% 
+  left_join(., xsex, by = "pidp") %>% #From script 6
+  left_join(., fb_check, by = "pidp") %>% 
+  mutate(fb = ifelse(is.na(fb), 0, fb))
 
-# Discrete time hazard model
+#First Imputation of permcon
+imp <-  mice(wide_permcon, m = 5, method = "logreg")
+densityplot(imp)
+fit <- with(imp, glm(sex ~ w2 + w4 + w6 + w8 + w10, family = binomial(link = "logit")))
+summary(pool(fit))
+
+#My fitness tests are low. It seems to me that the data is not truly missing at random.
+#Later tests seem to confirm this. The distributions are similar, but when using the MICE adding permcon
+#based on priors seems to take away the effect that simple line deletion has. Something is going on here.
+imp_wide_permcon = complete(imp, 4)
+
+imp_permcon<- imp_wide_permcon %>% 
+  #dplyr::select(-sex, -fb) %>% 
+  pivot_longer(cols = c("w2", "w4", "w6", "w8", "w10"), names_to = "wavename", values_to = "permcon.imp") %>%
+  group_by(pidp) %>% 
+  mutate(wave = row_number()) %>% 
+  mutate(wave = ifelse(wave == 5, 10, ifelse(wave == 4, 8, ifelse(wave == 3, 6, ifelse(wave == 2, 4, 2))))) %>% 
+  ungroup() %>% 
+  dplyr::select(-wavename) 
+
+
+############################################################################
+# Discrete time hazard model - employed -----------------------------------
+###########################################################################
 
 survemp %>% count(event2)  
 str(survemp)
-testglm <- glm(formula = event2 ~ t2_3 + agemn + agesq + se_ee + jbsec.dummy*permcon + edu_cat,
+testglm <- glm(formula = event2 ~ t2_3 + agemn + agesq + se_ee + jbsec.dummy*permcon.imp + edu_cat,
                family = binomial(link = "cloglog"),
-               data = survemp2)
+               data = survemp)
 summary(testglm)
 summ(testglm, exp = TRUE, scale = TRUE) #exp = TRUE means that we want exponentiated estimates
 
@@ -104,11 +141,22 @@ aov16 <- aov(hazard ~ permcon * jbsec.dummy, data=survemp2)
 summary(aov16)
 model.tables(aov16, type = "means")
 lm16 <- lm(hazard ~ permcon * jbsec.dummy, data=survemp2)
+
 summary(lm16)
 
 plot(allEffects(aov16), multiline=TRUE, ci.style="bars")
 
-#Interaction Test 3 - jbsec priv
+interaction.plot(x.factor = survmemp$permcon,
+                 trace.factor = survmemp$jbsec.dummy,
+                 response = survmemp$hazard)
+
+#Interaction for women
+interaction.plot(x.factor = survfemp$permcon,
+                 trace.factor = survfemp$jbsec.dummy,
+                 response = survfemp$hazard)
+
+
+###Interaction Test 3 - jbsec priv
 #
 interaction.plot(x.factor = survemp2$priv,
                  trace.factor = survemp2$jbsec.dummy,
@@ -147,26 +195,45 @@ plot(allEffects(aov18), multiline=TRUE, ci.style="bars")
 #############################################################################
 
 #Men Dataframe
-survmemp <- survemp2 %>% filter(edu_cat != "other", sex == 1)
+survmemp <- survemp %>% filter(edu_cat != "other", sex == 1)
 #Women Dataframe
-survfemp <- survemp2 %>% filter(edu_cat != "other", sex == 2)
+survfemp <- survemp %>% filter(edu_cat != "other", sex == 2)
 
-empmglm <- glm(formula = event2 ~ t2_3 + agemn + agesq + se_ee + jbsec.dummy + jbpl + edu_cat,
+empmglm <- glm(formula = event2 ~ t2_3 + agemn + agesq + se_ee + jbsec.dummy*permcon + edu_cat,
                family = binomial(link = "cloglog"),
                data = survmemp)
 summary(empmglm)
 summ(empmglm, exp = TRUE, scale = TRUE) #exp = TRUE means that we want exponentiated estimates
 
 
-empfglm <- glm(formula = event2 ~ t2_3 + agemn + agesq + se_ee +  jbsec.dummy + jbpl +  edu_cat,
+empfglm <- glm(formula = event2 ~ t2_3 + agemn + agesq + se_ee +  jbsec.dummy*permcon +  edu_cat,
                family = binomial(link = "cloglog"),
                data = survfemp)
 summary(empfglm)
 summ(empfglm, exp = TRUE, scale = TRUE) #exp = TRUE means that we want exponentiated estimates
 
+###########################################################################
+# Model output ------------------------------------------------------------
+###########################################################################
 
+#Descriptive stats
+statsurvemp <- survemp %>% 
+  group_by(pidp) %>% 
+  arrange(pidp, desc(wave)) %>% 
+  mutate(rev_time = row_number()) %>% 
+  filter(rev_time == 1) %>% 
+  #mutate(fb = ifelse(is.na(kdob), 0, 1)) %>% 
+  mutate(sex = as.factor(sex)) %>% 
+  mutate(sex = recode(sex,
+                      "1" = "Men",
+                      "2" = "Women"))
 
-
+mycontrols <- tableby.control(test = FALSE)
+desstats <-arsenal::tableby(fb ~ t2 + sex + se_ee + jbsec.dummy + permcon + edu_cat, data = statsurvemp, control = mycontrols)
+labels(desstats ) <-  c(sex = "Sex", se_ee = "PJI", finnow.imp = "Present Financial Outlook", finfut.imp = "Future Financial Outlook",
+                        jbsec = "Job Security", edu_cat = "Educational Attainment", combo = "Partnership, Partner's Job Status")
+summary(desstats )
+#write2word(desstats , "desstats .doc")
 
 
 ##########################################################################
